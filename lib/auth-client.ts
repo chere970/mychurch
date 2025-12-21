b9a663fd5594dcfd78d107ec55656458d466555c
cherinet <cherinetkebede055@gmail.com>
2025-12-21 23:47:38 +0300

    better-auth added

--- lib/auth-client.ts
+++ lib/auth-client.ts
@@ -4,7 +4,8 @@ import {
 
 
 export const authClient = createAuthClient({
-    baseURL: process.env.NEXT_PUBLIC_APP_URL,
+    baseURL: process.env.NEXT_PUBLIC_APP_URL || "http://localhost:3000",
+    
 
 })
 
--- lib/auth.ts
+++ lib/auth.ts
@@ -2,7 +2,10 @@ import {
     betterAuth
 } from 'better-auth';
 
+import Database from "better-sqlite3";
+
 export const auth = betterAuth({
+    database: new Database("./sqlite.db"),
     emailAndPassword: {
         enabled: true,
         async sendResetPassword(data, request) {
